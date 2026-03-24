fe9387166b3dfa943ad378f561627defeee48160
angelmartinez4 <angelmartinez200210@gmail.com>
2026-03-24 19:36:03 +0100

    Created network for Hybrid SAC

--- algorithm/discor/discor/network.py
+++ algorithm/discor/discor/network.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.nn.functional as F # mover
 from torch.distributions import Normal
 
 
@@ -95,7 +96,7 @@ class GaussianPolicy(BaseNetwork):
         normals = Normal(means, stds)
 
         # Sample actions.
-        xs = normals.rsample()
+        xs = normals.rsample() # equivalente a xs = mean + ruido_normal*std
         actions = torch.tanh(xs)
 
         # Calculate entropies.
@@ -103,3 +104,53 @@ class GaussianPolicy(BaseNetwork):
         entropies = -log_probs.sum(dim=1, keepdim=True)
 
         return actions, entropies, torch.tanh(means)
+
+
+class GaussianHybridPolicy(BaseNetwork):
+    LOG_STD_MAX = 2
+    LOG_STD_MIN = -20
+
+    def __init__(self, state_dim, action_continuous_dim, action_discrete_dims=[3], hidden_units=[256, 256]):
+        super().__init__()
+
+        self.net = create_linear_network(
+            input_dim=state_dim,
+            output_dim = hidden_units[-1],
+            hidden_units=hidden_units[:-1])
+
+        self.continuous_head = nn.Linear(hidden_units[-1], 2 * action_continuous_dim) # * 2, media/std
+        self.discrete_heads = []
+        for dim in action_discrete_dims: # initialize all discrete heads (one for discrete output)
+            self.discrete_heads.append(nn.Linear(hidden_units[-1], dim))
+
+        self.apply(initialize_weights_xavier)
+
+    def forward(self, states):
+        assert states.dim() == 2
+        features_hidden = self.net(states) # feature output of hidden layer
+
+        # --- Continuous head ---
+        # Calculate means and stds of actions.
+        means, log_stds = torch.chunk(features_hidden, 2, dim=-1)
+        log_stds = torch.clamp(
+            log_stds, min=self.LOG_STD_MIN, max=self.LOG_STD_MAX)
+        stds = log_stds.exp_()
+
+        # Gaussian distributions.
+        normals = Normal(means, stds)
+
+        # Sample actions.
+        xs = normals.rsample()  # equivalent to xs = mean + Normal(0,1).sample()*std
+        cont_actions = torch.tanh(xs)
+
+        # Calculate entropies.
+        cont_log_probs = normals.log_prob(xs) - torch.log(1 - cont_actions.pow(2) + 1e-6)
+        cont_entropies = -cont_log_probs.sum(dim=1, keepdim=True)
+
+        # --- Discrete head ---
+        discrete_probs = []
+        for head in self.discrete_heads:
+            logits = head(features_hidden)
+            discrete_probs.append(F.softmax(logits, dim=-1))
+
+        return cont_actions, cont_entropies, torch.tanh(means), discrete_probs
