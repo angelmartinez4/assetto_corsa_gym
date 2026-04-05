HSAC Critic Q-value calculation and policy algorithm fixes

algorithm/discor/discor/algorithm/hsac.py
@@ -9,8 +9,8 @@ from ..network import HybridTwinnedStateActionFunction, GaussianHybridPolicy
 from ..utils import disable_gradients, soft_update, update_params, \
     assert_action
 
-
 import logging
+
 logger = logging.getLogger(__name__)
 
 
@@ -31,19 +31,19 @@ class HSAC(Algorithm):
             action_cont_dim=self._action_cont_dim,
             action_disc_dims=self._action_disc_dims,
             hidden_units=policy_hidden_units
-            ).to(self._device)
+        ).to(self._device)
         self._online_q_net = HybridTwinnedStateActionFunction(
             state_dim=self._state_dim,
             action_cont_dim=self._action_cont_dim,
             action_disc_dims=self._action_disc_dims,
             hidden_units=q_hidden_units
-            ).to(self._device)
+        ).to(self._device)
         self._target_q_net = HybridTwinnedStateActionFunction(
             state_dim=self._state_dim,
             action_cont_dim=self._action_cont_dim,
             action_disc_dims=self._action_disc_dims,
             hidden_units=q_hidden_units
-            ).to(self._device).eval()
+        ).to(self._device).eval()
 
         # Copy parameters of the learning network to the target network.
         self._target_q_net.load_state_dict(self._online_q_net.state_dict())
@@ -58,9 +58,10 @@ class HSAC(Algorithm):
         # Target entropy is -|A_c|. (continuous)
         self._target_entropy_cont = -float(self._action_cont_dim)
 
-        # Target discrete entropy
-        self._target_entropy_disc = [0.5 * torch.log(torch.tensor(float(k)))
-                                     for k in self._action_disc_dims]
+        # Target discrete entropy. Each head has a different one, initialized based on size
+        self._target_entropy_disc = torch.tensor([0.5 * torch.log(torch.tensor(float(k)))
+                                                 for k in self._action_disc_dims],
+                                                 device=self._device)
 
         # We optimize log(alpha), instead of alpha.
         self._log_alpha_cont = torch.zeros(
@@ -79,14 +80,14 @@ class HSAC(Algorithm):
         state = torch.tensor(
             state[None, ...].copy(), dtype=torch.float, device=self._device)
         with torch.no_grad():
-            cont_actions, cont_entropies, _, disc_probs_list = self._policy_net(state)
+            cont_actions, cont_entropies, _, disc_probs_list, _ = self._policy_net(state)
 
         cont_actions = cont_actions.cpu().numpy()[0]
 
-        # choose best action (argmax from probs) for each discrete action
+        # sample action randomly (to explore!) following multinomial distribution
         disc_actions = [
-            probs.argmax(dim=-1).cpu().numpy()[0]
-            for probs in disc_probs_list
+            torch.multinomial(prob, num_samples=1).cpu().numpy()[0]  # binomial multiclass sampling for each action
+            for prob in disc_probs_list
         ]
 
         return cont_actions, disc_actions
@@ -95,7 +96,7 @@ class HSAC(Algorithm):
         state = torch.tensor(
             state[None, ...].copy(), dtype=torch.float, device=self._device)
         with torch.no_grad():
-            cont_actions, _, _, disc_probs_list = self._policy_net(state)
+            cont_actions, _, _, disc_probs_list, _ = self._policy_net(state)
         cont_actions = cont_actions.cpu().numpy()[0]
         assert_action(cont_actions)
 
@@ -121,19 +122,23 @@ class HSAC(Algorithm):
         states, actions, rewards, next_states, dones = batch
 
         # Update policy.
-        policy_loss, cont_entropies, disc_probs_list = self.calc_policy_loss(states)
+        policy_loss, cont_entropies, disc_entropies_list = self.calc_policy_loss(states)
         update_params(self._policy_optim, policy_loss)
 
         # Update the entropy coefficient.
         entropy_loss_cont = torch.zeros(1, device=self._device)
         entropy_loss_disc = torch.zeros(1, device=self._device)
         if self.update_entropy:
-            entropy_loss_cont, entropy_loss_disc = self.calc_entropy_loss(cont_entropies, disc_probs_list)
+            entropy_loss_cont, entropy_loss_disc = self.calc_entropy_loss(cont_entropies, disc_entropies_list)
             update_params(self._alpha_cont_optim, entropy_loss_cont)
             update_params(self._alpha_disc_optim, entropy_loss_disc)
             entropy_loss_cont = entropy_loss_cont.detach().item()
             entropy_loss_disc = entropy_loss_disc.detach().item()
+
         self._alpha_cont = self._log_alpha_cont.detach().exp()
+        self._alpha_disc = self._log_alpha_disc.detach().exp()
+        # calculate mean for logging, since disc_entropies is not a tensor
+        mean_disc_entropy = torch.stack(disc_entropies_list).mean().item()
 
         if self._learning_steps % self._log_interval == 0:
             writer.add_scalar(
@@ -146,19 +151,28 @@ class HSAC(Algorithm):
                 'loss/entropy_disc', entropy_loss_disc,
                 self._learning_steps)
             writer.add_scalar(
-                'stats/alpha', self._alpha_cont.item(),
+                'stats/alpha_cont', self._alpha_cont.item(),
                 self._learning_steps)
             writer.add_scalar(
-                'stats/entropy', cont_entropies.detach().mean().item(),
+                'stats/alpha_disc', self._alpha_disc.item(),
                 self._learning_steps)
+            writer.add_scalar(
+                'stats/entropy_cont', cont_entropies.detach().mean().item(),
+                self._learning_steps)
+            writer.add_scalar(
+                'stats/entropy_disc', mean_disc_entropy, self._learning_steps)
 
             return {"policy_loss": policy_loss.detach().item(),
-                    "entropy_loss": entropy_loss_cont,
-                    "alpha": self._alpha_cont.item(), "entropy": cont_entropies.detach().mean().item()}
+                    "entropy_loss_cont": entropy_loss_cont,
+                    "entropy_loss_disc": entropy_loss_disc,
+                    "alpha_cont": self._alpha_cont.item(),
+                    "alpha_disc": self._alpha_disc.item(),
+                    "entropy_cont": cont_entropies.detach().mean().item(),
+                    "entropy_disc": mean_disc_entropy}
 
-    def calc_policy_loss(self, states) -> tuple[torch.Tensor, torch.Tensor, list[torch.Tensor]]:
+    def calc_policy_loss(self, states):
         # Resample actions to calculate expectations of Q.
-        cont_actions, cont_entropies, _, disc_probs_list = self._policy_net(states)
+        cont_actions, cont_entropies, _, disc_probs_list, disc_entropies_list = self._policy_net(states)
 
         # Expectations of Q with clipped double Q technique.
         # each qi_list has a list for all discrete heads
@@ -166,27 +180,26 @@ class HSAC(Algorithm):
         # i.e. [[(q1,a0=0), (q1,a0=1)], [(q2, a1=-1), (q2, a1=0), (q2,a1=1)]
         q1_list, q2_list = self._online_q_net(states, cont_actions)
 
-        #policy_loss = 0.0
         policy_loss = torch.zeros(1, device=self._device)
+        # iterate over all discrete actions
+        for q1, q2, disc_probs, disc_entropy in zip(q1_list, q2_list, disc_probs_list, disc_entropies_list):
+            q_min = torch.min(q1, q2)  # (B, Ki). min(q1_d, q2_d) for each discrete action
+            # expected Q under policy + discrete entropy
+            qs_expected = (disc_probs * q_min).sum(dim=-1, keepdim=True)  # (B, 1). sum(pi*q)
 
-        for q1, q2, disc_probs in zip(q1_list, q2_list, disc_probs_list):
-            qs = torch.min(q1, q2)  # (B, Ki)
-            qs_expected = (disc_probs * qs).sum(dim=-1, keepdim=True)  # (B, 1)
-
-            disc_log_probs = torch.log(disc_probs + 1e-8)
-            disc_entropies = -(disc_probs * disc_log_probs).sum(dim=-1, keepdim=True)  # (B, 1)
-
-            # Maximizar Q_esperado + αd * H_discreta + αc * H_continua
-            assert qs_expected.shape == disc_entropies.shape == cont_entropies.shape
+            # maximize q_min + alpha_d * H_disc + alpha_c * H_cont
+            assert qs_expected.shape == disc_entropy.shape == cont_entropies.shape
             policy_loss += torch.mean(
                 -qs_expected
-                - self._alpha_disc * disc_entropies
+                - self._alpha_disc * disc_entropy
                 - self._alpha_cont * cont_entropies
             )
 
-        return policy_loss, cont_entropies.detach_(), [p.detach() for p in disc_probs_list]
+        policy_loss /= len(disc_probs_list)
 
-    def calc_entropy_loss(self, cont_entropies, disc_probs_list) -> tuple[torch.Tensor, torch.Tensor]:
+        return policy_loss, cont_entropies.detach_(), [e.detach() for e in disc_entropies_list]
+
+    def calc_entropy_loss(self, cont_entropies, disc_entropies_list) -> tuple[torch.Tensor, torch.Tensor]:
         assert not cont_entropies.requires_grad
 
         # Intuitively, we increse alpha when entropy is less than target
@@ -194,16 +207,13 @@ class HSAC(Algorithm):
         entropy_loss_cont = -torch.mean(
             self._log_alpha_cont * (self._target_entropy_cont - cont_entropies))
 
-        # discrete alphas, for each discrete head (action)
-        #entropy_loss_disc = 0.0
+        # discrete alphas, for each discrete head (discrete action)
+        # same as continuous but each discrete head has his own target entropy, that's why we iterate
         entropy_loss_disc = torch.zeros(1, device=self._device)
-        for i, disc_probs in enumerate(disc_probs_list):
-            disc_log_probs = torch.log(disc_probs)
-            disc_entropies = -(disc_probs * disc_log_probs).sum(dim=-1, keepdim=True)
-            assert not disc_entropies.requires_grad
-
+        for i, entropy in enumerate(disc_entropies_list): # iterate over different target entropies
+            assert not entropy.requires_grad
             entropy_loss_disc += -torch.mean(
-                self._log_alpha_disc * (self._target_entropy_disc[i] - disc_entropies))
+                self._log_alpha_disc * (self._target_entropy_disc[i] - entropy))
 
         return entropy_loss_cont, entropy_loss_disc
 
@@ -211,63 +221,86 @@ class HSAC(Algorithm):
         states, actions, rewards, next_states, dones = batch
 
         # Calculate current and target Q values.
-        curr_qs1, curr_qs2 = self.calc_current_qs(states, actions)
-        target_qs = self.calc_target_qs(rewards, next_states, dones)
+        # TODO fill batch with discrete actions and get them
+        curr_qs1_list, curr_qs2_list = self.calc_current_qs(states, actions)
+        target_qs_list = self.calc_target_qs(rewards, next_states, dones)
 
         # Update Q functions.
         q_loss, mean_q1, mean_q2 = \
-            self.calc_q_loss(curr_qs1, curr_qs2, target_qs, imp_ws1, imp_ws2)
+            self.calc_q_loss(curr_qs1_list, curr_qs2_list, target_qs_list, imp_ws1, imp_ws2)
         update_params(self._q_optim, q_loss)
 
         if self._learning_steps % self._log_interval == 0:
             writer.add_scalar(
-                'loss/Q', q_loss.detach().item(),
-                self._learning_steps)
+                'loss/Q', q_loss.detach().item(), self._learning_steps)
             writer.add_scalar(
                 'stats/mean_Q1', mean_q1, self._learning_steps)
             writer.add_scalar(
                 'stats/mean_Q2', mean_q2, self._learning_steps)
 
         # Return there values for DisCor algorithm.
-        return curr_qs1.detach(), curr_qs2.detach(), target_qs
+        return curr_qs1_list, curr_qs2_list, target_qs_list
 
-    def calc_current_qs(self, states, actions):
-        curr_qs1, curr_qs2 = self._online_q_net(states, actions)
+    def calc_current_qs(self, states, actions, disc_actions_list):
+        q1_list, q2_list = self._online_q_net(states, actions)
+
+        curr_qs1, curr_qs2 = [], []
+        for i, (q1, q2) in enumerate(zip(q1_list, q2_list)):
+            idx = disc_actions_list[i].long().unsqueeze(-1) # test dimensions @TODO
+            curr_qs1.append(q1.gather(1, idx))
+            curr_qs2.append(q2.gather(1, idx))
+
         return curr_qs1, curr_qs2
 
     def calc_target_qs(self, rewards, next_states, dones):
         with torch.no_grad():
-            next_actions, next_entropies, _ = self._policy_net(next_states)
-            next_qs1, next_qs2 = self._target_q_net(next_states, next_actions)
-            next_qs = \
-                torch.min(next_qs1, next_qs2) + self._alpha_cont * next_entropies
+            next_cont_actions, next_cont_entropies, _, next_disc_probs, _ = self._policy_net(next_states)
+            next_qs1_list, next_qs2_list = self._target_q_net(next_states, next_cont_actions)
 
-        assert rewards.shape == next_qs.shape
-        target_qs = rewards + (1.0 - dones) * self._discount * next_qs
+            target_qs_list = []
+            q_cont = self._alpha_cont * next_cont_entropies  # From original SAC
+            for q1, q2, disc_probs in zip(next_qs1_list, next_qs2_list, next_disc_probs):
+                q_min = torch.min(q1, q2)  # [B, K_di]. K_di: number of values of discrete action i
+                disc_log_probs = torch.log(disc_probs + 1e-8)
 
-        return target_qs
+                # expected = Q'(s, ac) + alpha_d * Hd + alpha_c * Hc
+                # Q'(s, ac) = sum(probs * q_min). Hd = -sum(alpha_d * probs * log_probs)
+                # fuse the sums: sum(probs * (q_min - alpha_d * log_probs))
+                q_disc = torch.sum(disc_probs * (q_min - self._alpha_disc * disc_log_probs), dim=1, keepdim=True)
 
-    def calc_q_loss(self, curr_qs1, curr_qs2, target_qs, imp_ws1=None,
-                    imp_ws2=None):
-        assert imp_ws1 is None or imp_ws1.shape == curr_qs1.shape
-        assert imp_ws2 is None or imp_ws2.shape == curr_qs2.shape
-        assert not target_qs.requires_grad
-        assert curr_qs1.shape == target_qs.shape
+                q_next = q_disc + q_cont
+                target_qs = rewards + (1 - dones) * self._discount * q_next
+                target_qs_list.append(target_qs)
 
-        # Q loss is mean squared TD errors with importance weights.
-        if imp_ws1 is None:
-            q1_loss = torch.mean((curr_qs1 - target_qs).pow(2))
-            q2_loss = torch.mean((curr_qs2 - target_qs).pow(2))
+        return target_qs_list
 
-        else:
-            q1_loss = torch.sum((curr_qs1 - target_qs).pow(2) * imp_ws1)
-            q2_loss = torch.sum((curr_qs2 - target_qs).pow(2) * imp_ws2)
+    def calc_q_loss(self, curr_qs1_list, curr_qs2_list, target_qs_list,
+                    imp_ws1=None, imp_ws2=None):
+        assert len(curr_qs1_list) == len(curr_qs2_list) > 0
 
-        # Mean Q values for logging.
-        mean_q1 = curr_qs1.detach().mean().item()
-        mean_q2 = curr_qs2.detach().mean().item()
+        n = len(curr_qs1_list)
+        q_loss = torch.zeros(1, device=self._device)
+        mean_q1, mean_q2 = 0.0, 0.0
+        for curr_qs1, curr_qs2, target_qs in zip(curr_qs1_list, curr_qs2_list, target_qs_list):
+            assert not target_qs.requires_grad
+            assert curr_qs1.shape == target_qs.shape == curr_qs2.shape
+            assert imp_ws1 is None or imp_ws1.shape == curr_qs1.shape
+            assert imp_ws2 is None or imp_ws2.shape == curr_qs2.shape
 
-        return q1_loss + q2_loss, mean_q1, mean_q2
+            # Q loss is mean squared TD errors with importance weights.
+            if imp_ws1 is None:
+                q1_loss = torch.mean((curr_qs1 - target_qs).pow(2))
+                q2_loss = torch.mean((curr_qs2 - target_qs).pow(2))
+            else:
+                q1_loss = torch.sum((curr_qs1 - target_qs).pow(2) * imp_ws1)
+                q2_loss = torch.sum((curr_qs2 - target_qs).pow(2) * imp_ws2)
+
+            q_loss += q1_loss + q2_loss
+            # Mean Q values for logging.
+            mean_q1 = curr_qs1.detach().mean().item()
+            mean_q2 = curr_qs2.detach().mean().item()
+
+        return q_loss, mean_q1 / n, mean_q2 / n
 
     def save_models(self, save_dir):
         super().save_models(save_dir)

algorithm/discor/discor/network.py
@@ -116,7 +116,8 @@ class GaussianHybridPolicy(BaseNetwork):
         self.net = create_linear_network(
             input_dim=state_dim,
             output_dim = hidden_units[-1],
-            hidden_units=hidden_units[:-1])
+            hidden_units=hidden_units[:-1],
+            output_activation=None) # Trunk output is raw logits
 
         self.continuous_head = nn.Linear(hidden_units[-1], 2 * action_cont_dim) # * 2, mean/std for each cont feat
         self.discrete_heads = nn.ModuleList([
@@ -148,12 +149,18 @@ class GaussianHybridPolicy(BaseNetwork):
         cont_entropies = -cont_log_probs.sum(dim=1, keepdim=True)
 
         # --- Discrete head ---
-        discrete_probs = []
-        for head in self.discrete_heads:
+        disc_probs = []
+        disc_entropies = []
+        for head in self.discrete_heads: # have to iterate since heads can have different size
             logits = head(features_hidden)
-            discrete_probs.append(F.softmax(logits, dim=-1))
+            probs = F.softmax(logits, dim=-1)
+            disc_probs.append(probs)
 
-        return cont_actions, cont_entropies, torch.tanh(means), discrete_probs
+            # Discrete entropies: -sum(probs * log(probs))
+            disc_entropy = -(probs * torch.log(probs + 1e-8)).sum(dim=1, keepdim=True)
+            disc_entropies.append(disc_entropy)
+
+        return cont_actions, cont_entropies, torch.tanh(means), disc_probs, disc_entropies
 
 
 class HybridStateActionFunction(BaseNetwork): # Critic NN
@@ -164,7 +171,8 @@ class HybridStateActionFunction(BaseNetwork): # Critic NN
         self.net = create_linear_network(
             input_dim=state_dim+action_cont_dim, # Critic input: state and continuous actions. NOT discrete
             output_dim=hidden_units[-1],
-            hidden_units=hidden_units[:-1])
+            hidden_units=hidden_units[:-1],
+            output_activation=None) # Trunk output is raw logits
 
         self.discrete_q_heads = nn.ModuleList([
             nn.Linear(hidden_units[-1], dim) for dim in action_disc_dims
@@ -172,10 +180,10 @@ class HybridStateActionFunction(BaseNetwork): # Critic NN
 
     def forward(self, x):
         features = self.net(x)
-        return [head(features) for head in self.discrete_q_heads]
+        return [head(features) for head in self.discrete_q_heads] # q_value for each discrete action/head
 
 
-class HybridTwinnedStateActionFunction(BaseNetwork):
+class HybridTwinnedStateActionFunction(BaseNetwork): # Critic has two NN for Double Q Clipping technique
 
     def __init__(self, state_dim, action_cont_dim, action_disc_dims=[3], hidden_units=[256, 256]):
         super().__init__()
@@ -187,7 +195,7 @@ class HybridTwinnedStateActionFunction(BaseNetwork):
         assert states.dim() == 2 and cont_actions.dim() == 2
 
         x = torch.cat([states, cont_actions], dim=1)
-        q1_list = self.net1(x)
-        q2_list = self.net2(x)
+        q1_list = self.net1(x) # q_value for each discrete action
+        q2_list = self.net2(x) # q_value for each discrete action
 
         return q1_list, q2_list